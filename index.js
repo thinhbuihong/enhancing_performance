25ea6bf5977d46676cdaddac9d0513d7065cb768
thinh <thinhbuihong.ma@gmail.com>
2022-04-01 20:55:31 +0700

    choose number of cluster

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// process.env.UV_THREADPOOL_SIZE = 1; //1 thread per cluster
+
 const cluster = require('cluster');
+const crypto = require('crypto')
 
 if (cluster.isMaster) {
 	const numberOfCluster = 4
@@ -9,14 +12,21 @@ if (cluster.isMaster) {
 	const express = require('express');
 	const app = express();
 
-	function doWork(duration) {
-		const start = Date.now();
-		while (Date.now() - start < duration) { }
-	}
+	// function doWork(duration) {
+	// 	const start = Date.now();
+	// 	while (Date.now() - start < duration) { }
+	// }
+
+	// const start = Date.now()
+	// crypto.pbkdf2('a','b',100000,512,'sha512',()=>{
+	// 	console.log("1:",Date.now()-start);
+	// })
 
 	app.get('/', (req, res) => {
-		doWork(10000)
-		res.send('hi there')
+		// doWork(10000)
+		crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+			res.send('hi there')
+		})
 	})
 
 	app.get('/fast', (req, res) => {
